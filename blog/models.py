e404167418029602588ed0dc839aadd26ac5fb8b
Anthony Torres <atorres@g2crowd.com>
2018-04-06 21:57:46 -0500

    Modify

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.utils.encoding import smart_text
 from django.utils import timezone
-from .validators import validate_justin, validate_author_email
+from django.utils.text import slugify
+from .validators import validate_justin
 
 PUBLISH_CHOICES = (
         ('draft', 'Draft'),
@@ -14,11 +15,18 @@ class PostModel(models.Model):
     id = models.BigAutoField(primary_key=True)
     active = models.BooleanField(default=True)
     title = models.CharField(max_length=255, default='New Title', unique=True)
+    slug = models.SlugField(null=True, blank=True)
     content = models.TextField(null=True, blank=True)
     publish = models.CharField(max_length=120, default='draft', choices=PUBLISH_CHOICES)
     view_count = models.IntegerField(default=0)
     publish_data = models.DateField(auto_now=False, auto_now_add=False, default=timezone.now)
     author_email = models.EmailField(max_length=240, null=True, blank=True, validators=[validate_justin])
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+
+        super(PostModel, self).save(*args, **kwargs)
 
     class Meta:
         verbose_name = 'Post'
